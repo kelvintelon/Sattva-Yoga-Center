9285acf26e8d289dbc84f8a383b140c0ad0e995f
Kelvin <kelvin.telon@techelevator.com>
2022-12-20 14:22:57 -0500

    vuetify

---
+++ vue/src/components/LoginButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <v-btn elevation="5">
+      <v-icon > mdi-login
+      </v-icon> Login
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login-button",
+  data() {
+    return {};
+  },
+  created() {},
+};
+</script>
+
+<style>
+
+
+</style>
