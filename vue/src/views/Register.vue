df41012aafbb0c700388cda5c5f1f07c95325e6b
Kelvin <kelvin.telon@techelevator.com>
2022-12-22 10:56:14 -0500

    register page additional styling

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,75 +1,93 @@
 <template>
   <div id="register" class="text-center">
-    <form class="form-register" @submit.prevent="register">
-      <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
-      <div class="alert alert-danger" role="alert" v-if="registrationErrors">
-        {{ registrationErrorMsg }}
-      </div>
-      <label for="username" class="sr-only">Username</label>
-      <input
-        type="text"
-        id="username"
-        class="form-control"
-        placeholder="Username"
-        v-model="user.username"
-        required
-        autofocus
-      />
-      <label for="password" class="sr-only">Password</label>
-      <input
-        type="password"
-        id="password"
-        class="form-control"
-        placeholder="Password"
-        v-model="user.password"
-        required
-      />
-      <input
-        type="password"
-        id="confirmPassword"
-        class="form-control"
-        placeholder="Confirm Password"
-        v-model="user.confirmPassword"
-        required
-      />
-      <router-link :to="{ name: 'login' }">Have an account?</router-link>
-      <button class="btn btn-lg btn-primary btn-block" type="submit">
-        Create Account
-      </button>
-    </form>
+    <v-container>
+      <header-logo></header-logo>
+      <v-row><v-col></v-col></v-row>
+      <v-row>
+        <v-spacer></v-spacer>
+        <v-col cols=4>
+          <v-form class="form-register" @submit.prevent="register">
+            <h1 class="h3 mb-3 font-weight-normal">Create Account</h1>
+            <div
+              class="alert alert-danger"
+              role="alert"
+              v-if="registrationErrors"
+            >
+              {{ registrationErrorMsg }}
+            </div>
+            <label for="username" class="sr-only">Username</label>
+            <input
+              type="text"
+              id="username"
+              class="form-control"
+              placeholder="Username"
+              v-model="user.username"
+              required
+              autofocus
+            />
+            <label for="password" class="sr-only">Password</label>
+            <input
+              type="password"
+              id="password"
+              class="form-control"
+              placeholder="Password"
+              v-model="user.password"
+              required
+            />
+            <input
+              type="password"
+              id="confirmPassword"
+              class="form-control"
+              placeholder="Confirm Password"
+              v-model="user.confirmPassword"
+              required
+            />
+            <router-link :to="{ name: 'login' }">Have an account?</router-link>
+            <v-btn class="btn btn-lg btn-primary btn-block" type="submit">
+              Create Account
+            </v-btn>
+          </v-form>
+        </v-col>
+        <v-spacer></v-spacer>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-import authService from '../services/AuthService';
+import authService from "../services/AuthService";
+import HeaderLogo from "../components/HeaderLogo.vue";
 
 export default {
-  name: 'register',
+  name: "register",
+   components: {
+    HeaderLogo,
+   },
   data() {
     return {
       user: {
-        username: '',
-        password: '',
-        confirmPassword: '',
-        role: 'user',
+        username: "",
+        password: "",
+        confirmPassword: "",
+        role: "user",
       },
       registrationErrors: false,
-      registrationErrorMsg: 'There were problems registering this user.',
+      registrationErrorMsg: "There were problems registering this user.",
     };
   },
   methods: {
     register() {
       if (this.user.password != this.user.confirmPassword) {
         this.registrationErrors = true;
-        this.registrationErrorMsg = 'Password & Confirm Password do not match.';
+        this.registrationErrorMsg = "Password & Confirm Password do not match.";
       } else {
         authService
           .register(this.user)
           .then((response) => {
             if (response.status == 201) {
               this.$router.push({
-                path: '/login',
-                query: { registration: 'success' },
+                path: "/login",
+                query: { registration: "success" },
               });
             }
           })
@@ -77,17 +95,24 @@ export default {
             const response = error.response;
             this.registrationErrors = true;
             if (response.status === 400) {
-              this.registrationErrorMsg = 'Bad Request: Validation Errors';
+              this.registrationErrorMsg = "Bad Request: Validation Errors";
             }
           });
       }
     },
     clearErrors() {
       this.registrationErrors = false;
-      this.registrationErrorMsg = 'There were problems registering this user.';
+      this.registrationErrorMsg = "There were problems registering this user.";
     },
   },
 };
 </script>
 
-<style></style>
+<style>
+
+.form-register {
+  display:flex;
+  flex-direction: column;
+}
+
+</style>
