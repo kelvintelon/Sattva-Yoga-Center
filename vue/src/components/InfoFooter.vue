a78f5c2ad8807dc79e396ebcf8741d926a5f778e
Sean Oberc <seanoberc@gmail.com>
2022-12-23 10:59:28 -0500

    temp fix: footer placeholder

--- vue/src/components/InfoFooter.vue
+++ vue/src/components/InfoFooter.vue
@@ -37,29 +37,10 @@ export default {
       'mdi-home',
       'mdi-email',
       'mdi-calendar',
-      'mdi-delete',
     ],
-    items: [
-      'default',
-      'absolute',
-      'fixed',
-    ],
-    padless: false,
-    variant: 'default',
-  }),
-  computed: {
-    localAttrs () {
-      const attrs = {}
 
-      if (this.variant === 'default') {
-        attrs.absolute = false
-        attrs.fixed = false
-      } else {
-        attrs[this.variant] = true
-      }
-      return attrs
-    },
-  },
+  }),
+
 }
 </script>
 
