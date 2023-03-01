a3687ad085fe0ba414bb3e51236ab48137ac751c
Sean Oberc <sean.oberc@techelevator.com>
2023-03-01 09:11:46 -0500

    change: header to app nav bar

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,52 +1,25 @@
 <template>
   <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-layout>
+      <NavDrawer>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      </NavDrawer>
+      <v-main>
+        <router-view/>
+      </v-main>
 
-      <v-spacer></v-spacer>
+    </v-layout>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
-
-    <v-main>
-      <router-view/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
 
+import NavDrawer from "@/layout/NavDrawer.vue";
+
 export default {
   name: 'App',
+  components: {NavDrawer},
 
   data: () => ({
     //
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,38 +1,19 @@
 <template>
   <v-container>
-    <top-header></top-header>
-    <v-row class="mb-lg-16">
-      <v-col cols="4">
-        <home-card></home-card>
-      </v-col>
-      <v-col cols="4">
-        <welcome-card></welcome-card>
-      </v-col>
-      <v-col cols="4">
-        <about-card></about-card>
-      </v-col>
-    </v-row>
-    <info-footer></info-footer>
+
+ <home-card></home-card>
+
   </v-container>
 </template>
 
 <script>
 
 import HomeCard from "../components/HomeCard.vue";
-import TopHeader from '../components/TopHeader.vue';
-import WelcomeCard from "@/components/WelcomeCard.vue";
-import AboutCard from "@/components/AboutCard.vue";
-import InfoFooter from "@/components/InfoFooter.vue";
-
-
 export default {
   name: "home",
   components: {
-    InfoFooter,
-    AboutCard,
-    WelcomeCard,
     HomeCard,
-    TopHeader,
+
 
   },
 };
