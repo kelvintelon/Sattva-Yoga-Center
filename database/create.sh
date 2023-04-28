85e678bf92b35e3c85595ccb7df55371862f3f26
Kelvin Telon <telon.kelvin77@gmail.com>
2023-04-28 09:34:35 -0400

    changed the script to include the admin and user

--- database/create.sh
+++ database/create.sh
@@ -5,5 +5,6 @@ DATABASE=yoga_db
 psql -U postgres -f "$BASEDIR/dropdb.sql" &&
 createdb -U postgres $DATABASE &&
 psql -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql"
+psql -U postgres -d $DATABASE -f "$BASEDIR/data.sql" &&
+psql -U postgres -d $DATABASE -f "$BASEDIR/newUsers.sql"
 
--- database/newUsers.sql
+++ database/newUsers.sql
@@ -2,5 +2,4 @@ BEGIN TRANSACTION;
 
 INSERT INTO users (username,password_hash,role,activated) VALUES ('user','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_USER',true);
 INSERT INTO users (username,password_hash,role,activated) VALUES ('admin','$2a$08$UkVvwpULis18S19S5pZFn.YHPZt3oaqHZnDwqbCW9pft6uFtkXKDC','ROLE_ADMIN',true);
-​
 COMMIT TRANSACTION;
